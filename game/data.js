bddb7f1445cae08e88eb2f099ba53283f26a4fa9
hilma <yotam.hanoch@gmail.com>
2022-11-23 17:23:15 +0200

    new button

--- game/data.js
+++ game/data.js
@@ -1,37 +1,4 @@
-let questions = [
-    {
-      question: 'What is 2 + 2?',
-      answers: [
-        { text: '4', correct: true },
-        { text: '22', correct: false }
-      ]
-    },
-    {
-      question: 'Who is the best YouTuber?',
-      answers: [
-        { text: 'Web Dev Simplified', correct: true },
-        { text: 'Traversy Media', correct: true },
-        { text: 'Dev Ed', correct: true },
-        { text: 'Fun Fun Function', correct: true }
-      ]
-    },
-    {
-      question: 'Is web development fun?',
-      answers: [
-        { text: 'Kinda', correct: false },
-        { text: 'YES!!!', correct: true },
-        { text: 'Um no', correct: false },
-        { text: 'IDK', correct: false }
-      ]
-    },
-    {
-      question: 'What is 4 * 2?',
-      answers: [
-        { text: '6', correct: false },
-        { text: '8', correct: true }
-      ]
-    }
-  ]
+let questions = []
 
 const question = document.getElementById('question');
 const answer1 = document.getElementById('answer1');
@@ -39,8 +6,9 @@ const answer2 = document.getElementById('answer2');
 const answer3 = document.getElementById('answer3');
 const answer4 = document.getElementById('answer4');
 const add = document.getElementById('add');
+const add1 = document.getElementById('add1');
 const inputs = document.querySelectorAll('input')
-
+add1.addEventListener('click',sendToLocalStorage);
 add.addEventListener('click', createNewQuestion);
 
 function returnToGame(){
@@ -50,8 +18,8 @@ window.location.href = '/game/game.html'
 
 function sendToLocalStorage(){
     add.removeEventListener('click',  sendToLocalStorage)
-    add.addEventListener('click', returnToGame)
-    add.value = 'חזור למשחק'
+    add1.addEventListener('click', returnToGame)
+    add1.value = 'חזור למשחק'
     console.log(questions)
     if(localStorage.getItem('allQuestions') === ''){
         localStorage.setItem('allQuestions',JSON.stringify(questions))
@@ -74,7 +42,7 @@ function createNewQuestion(){
        }
     if(questions.length === 2){
         add.removeEventListener('click', createNewQuestion)
-        add.value = 'הוסף שאלות למאגר'
+        // add.value = 'הוסף שאלות למאגר'
         alert('לא ניתן להוסיף יותר שאלות')
    
         add.addEventListener('click', sendToLocalStorage)
