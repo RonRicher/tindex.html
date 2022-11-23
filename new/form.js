1a81524174eca376947de32c6696aba8db42d950
hilma <elyasaf11@gmail.com>
2022-11-23 15:17:27 +0200

    forms correct

--- new/form.js
+++ new/form.js
@@ -1,30 +1,43 @@
-let submit = document.querySelector('#submit');
 let usersArr = [];
+
+
+
+let submit = document.getElementById('submit');
 submit.addEventListener('click', summary);
+console.log("whattt");
 
 
-function summary(){
-    
+function summary(event){
+
+    event.preventDefault();
     let username = document.getElementById('userName').value;
     let password = document.getElementById('password').value;
-    objectFactory(username, password);
-    windows.location='/new/main.html'
-    console.log(document.getElementById('userName').value)
-    console.log(document.getElementById('password').value)
+    objectFactory(username, password)
+    
+   
 }
 
 function objectFactory(username, password){
-    
     let object = {
         userName: username,
         passWord: password
     }
-    for(let i = 0; i < usersArr.length; i++){
-        if(this.userName === usersArr[i].userName){
+
+    let userArrFromStorage = localStorage.getItem('usersArr');
+    if (userArrFromStorage !== null){
+        usersArr = JSON.parse(userArrFromStorage);
+    
+        const checkExistUser = usersArr.some(user => user.userName === username)
+        if(checkExistUser){
+            document.getElementById('existUserName').textContent = "UserName is already exist" ;
             return false;
         }
     }
-
     usersArr.push(object);
-    
+    localStorage.setItem('usersArr', JSON.stringify(usersArr));
+    window.location.href= '/new/main.html';
+    // return true;
 }
+// function changePage(){
+//     windows.location.replace ='/new/'
+// }
